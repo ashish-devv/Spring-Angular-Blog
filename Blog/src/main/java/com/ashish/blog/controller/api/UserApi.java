b033d248824ae348661a4541e5d41404fd9a518f
ashish-devv <akashish908@gmail.com>
2020-11-14 19:49:38 +0530

    like Feature Logic Added.

--- Blog/src/main/java/com/ashish/blog/controller/api/UserApi.java
+++ Blog/src/main/java/com/ashish/blog/controller/api/UserApi.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ashish.blog.dao.Likerepo;
 import com.ashish.blog.dao.Postrepo;
+import com.ashish.blog.entity.Like;
 import com.ashish.blog.entity.Post;
 
 @RestController
@@ -19,6 +21,9 @@ public class UserApi {
 	
 	@Autowired
 	Postrepo postrepo;
+	
+	@Autowired
+	Likerepo likerepo;
 	
 	@GetMapping("/allpost")
 	public List<Post> getallpost()
@@ -41,10 +46,28 @@ public class UserApi {
 	public String getlikeinfo(@PathVariable("pid") int pid,HttpSession httpSession)
 	{
 		try {
+			int uid=(int) httpSession.getAttribute("uid");
 			Post post=this.postrepo.findByPid(pid);
 			if(post!=null)
 			{
-				return post.toString();
+				Like l=this.likerepo.getlikeinfobypostid(pid,uid);
+				if(l==null)
+				{
+					Like newlike = new Like();
+					newlike.setPostid(pid);
+					newlike.setUserid(uid);
+					//System.out.println(newlike);
+					this.likerepo.save(newlike);
+					int count = this.likerepo.countBypid(pid);
+					return count+"<a class='btn btn-danger'href='/user/api/like/"+pid+"'>👎</a>";
+				}
+				else
+				{
+					this.likerepo.deleteById(l.getLid());
+					int count = this.likerepo.countBypid(pid);
+					return count+"<a class='btn btn-success'href='/user/api/like/"+pid+"'>👍</a>";
+				}
+				
 			}
 			else
 			{
@@ -55,6 +78,21 @@ public class UserApi {
 			return e.getMessage();
 		}
 		
+	}
+	
+	
+	@GetMapping("/likecount/{pid}")
+	public String getlikecount(@PathVariable("pid") int pid)
+	{
+		try {
+			int count=this.likerepo.countBypid(pid);
+			return ""+count;
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
 	}
 
 }
--- Blog/src/main/java/com/ashish/blog/dao/Likerepo.java
+++ Blog/src/main/java/com/ashish/blog/dao/Likerepo.java
@@ -1,9 +1,17 @@
 package com.ashish.blog.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ashish.blog.entity.Like;
 
 public interface Likerepo extends JpaRepository<Like, Integer>{
+	
+	@Query("select l from Like l where l.postid= :pid and l.userid= :uid")
+	public Like getlikeinfobypostid(@Param("pid") int pid,@Param("uid") int uid);
+	
+	@Query("select COUNT(l) from Like l where l.postid= :pid")
+	public int countBypid(@Param("pid") int pid);
 
 }
