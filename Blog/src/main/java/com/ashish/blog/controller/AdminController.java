521a07bdcafa709c47d53fed80b0e83aec7d3ff1
ashish-devv <akashish908@gmail.com>
2020-12-04 15:57:27 +0530

    git testing

--- Blog/src/main/java/com/ashish/blog/controller/AdminController.java
+++ Blog/src/main/java/com/ashish/blog/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.ashish.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 	
 	@GetMapping("/")
-	public String adminindex()
+	public String adminindex(Model model)
 	{
 		return "admin/adminindex";
 	}
